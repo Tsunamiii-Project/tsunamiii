dc6ee8202f1e2301960ed798c3967e50ab7bac88
Limerio <44976231+Limerio@users.noreply.github.com>
2023-02-26 13:40:04 +0100

    Add open api documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import type { NestExpressApplication } from '@nestjs/platform-express'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import connectRedis from 'connect-redis'
 import session from 'express-session'
 import * as passport from 'passport'
@@ -10,6 +11,8 @@ import { AppModule } from './app.module'
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule)
 	const port = process.env.PORT
+
+	// auth part
 	const RedisStore = connectRedis(session)
 	const redisClient = createClient({ legacyMode: true })
 	await redisClient.connect()
@@ -28,6 +31,14 @@ async function bootstrap() {
 	)
 	app.use(passport.initialize())
 	app.use(passport.session())
+
+	// docs openapi
+	const config = new DocumentBuilder()
+		.setTitle('Api')
+		.setVersion('1.0')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('docs', app, document)
 
 	await app.listen(port)
 	Logger.log(`🚀 Application API is running on: http://localhost:${port}`)
